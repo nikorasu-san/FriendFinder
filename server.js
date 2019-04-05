0a0dcc301e3323749ed0bcd936dac294476e46b3
Liz Lemon <nikorasu.dx@gmail.com>
2019-04-04 22:46:22 -0500

    built some web pages

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// run route functions
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Starts the server to begin listening (Always put after code)
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
