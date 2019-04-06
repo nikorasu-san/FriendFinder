6adf091f606725b8d8e9bcb714038d03d8827305
Liz Lemon <nikorasu.dx@gmail.com>
2019-04-06 00:51:27 -0500

    comparison work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,7 +13,7 @@ module.exports = function (app) {
     });
 
     app.get("/api/users", function (req, res) {
-        connection.query("SELECT * FROM ??;", "users", function (err, data) {
+        connection.query("SELECT * FROM users;", function (err, data) {
             if (err) throw err;
             //console.log(data)
             res.json(data);
@@ -22,44 +22,62 @@ module.exports = function (app) {
 
 
     app.post("/api/users", function (req, res) {
-        debugger
-        console.log(req.body)
+        var userData = req.body
+        console.log(userData)
+        // compare user score with tv shows
+        connection.query("SELECT show_name, score FROM shows;", function (err, shows) {
+            if (err) throw err;
+            console.log(shows)
+            const bestMatch = {
+                show_name: '',
+                scoreDifference: 100
+                // photo: 'url',
+                // friendDifference: Infinity
+            }
+            for (let i = 0; i < shows.length; i++) {
+                totalDifference = 0;
+                currentShow = shows[i]
+                currentScore = currentShow.score.split(",")
+                for (let j = 0; j < currentScore.length; j++) {
+                    totalDifference += Math.abs(parseInt(userData.score[j]) - parseInt(currentScore[j]))
+                    console.log(currentShow.show_name, totalDifference)
+                }
+                if (totalDifference < bestMatch.scoreDifference) {
+                    bestMatch.show_name = currentShow.show_name;
+                    bestMatch.scoreDifference = totalDifference
+                    totalDifference = 0;
+                } else {
+                    totalDifference = 0
+                }
 
-        console.log(res)
-        var newUser = req.body
-        console.log("nop", newUser)
-        //     compare users
-        /*
-    
-           function findMatch(userData, friends) {
-        const bestMatch = {
-            name: '',
-            photo: 'url',
-            friendDifference: Infinity
-        }
-    
-        for (let i loop over the friends) {
-            totalDifference = 0;
-            currentFriend = friends[i]
-    
-            for (let j loop over currentFriend.scores){
-                //compare the userData.scores[j] and currentFriend.scores[j]
-    
-                totalDifference += Math.abs(parseInt(user score) - parseInt(friend score))
             }
-    
-    if (totalDifference <= bestMatch.friendDifference) {
-                bestMatch = currentFriend
-            }
-    
-            
-        }
-    
-    return bestMatch;
-    }
-    
-    }
-    */
+            console.log("best:", bestMatch)
+
+
+
+        })
+        //     for (let i loop over the friends) {
+        //         totalDifference = 0;
+        //         currentFriend = friends[i]
+
+        //         for (let j loop over currentFriend.scores){
+        //             //compare the userData.scores[j] and currentFriend.scores[j]
+
+        //             totalDifference += Math.abs(parseInt(user score) - parseInt(friend score))
+        //         }
+
+        // if (totalDifference <= bestMatch.friendDifference) {
+        //             bestMatch = currentFriend
+        //         }
+
+
+        //     }
+
+        // return bestMatch;
+        // }
+
+        // }
+
 
         res.json(true)
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Dependencies
 var express = require("express");
-var path = require("path");
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(express.static('public'));
@@ -8,14 +8,15 @@ app.use(express.static('public'));
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 3000;
 
-// run route functions
-
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// run route functions
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // Starts the server to begin listening (Always put after code)
 // =============================================================
